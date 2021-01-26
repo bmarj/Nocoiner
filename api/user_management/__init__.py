a86fa3c93d7ab3efd32b605b37079422974f23c1
Branimir <Branimir@BRANE-G5>
2021-01-26 13:56:57 +0100

    cleanup init

--- api/user_management/__init__.py
+++ api/user_management/__init__.py
@@ -1,17 +1,5 @@
-from flask import redirect
-from flask.helpers import url_for
-from flask_login import LoginManager, login_required
-from .business import get_user_by_id
+from flask_login import login_required
 from .blueprint import UserManager
 
 
-
 um = UserManager()
-
-# @um.user_loader
-# def load_user(id):
-#     return get_user_by_id(id)
-
-# @um.unauthorized_handler
-# def unauthorized():
-#     return redirect(url_for('user_management.login'))
