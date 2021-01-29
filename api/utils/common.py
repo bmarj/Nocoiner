62721c9621bd1a9b5bb49bede0879e556af780bf
Branimir <Branimir@BRANE-G5>
2021-01-29 15:35:59 +0100

    unique validation for editing

--- api/utils/common.py
+++ api/utils/common.py
@@ -1,13 +1,13 @@
 from flask import request, render_template, flash
 
 
-def generic_add(formClass, template, id=None, submit_target=None):
-    obj = formClass.Meta.model()
+def generic_add(form_class, template, id=None, submit_target=None):
+    obj = form_class.Meta.model()
 
     if request.method == 'GET':
-        form = formClass(obj=obj)
+        form = form_class(obj=obj)
     else:
-        form = formClass(request.values)
+        form = form_class(request.values)
 
     if form.validate_on_submit():        
         form.populate_obj(obj)
@@ -16,32 +16,34 @@ def generic_add(formClass, template, id=None, submit_target=None):
         flash('Data saved', category="Success")
         return render_template("form_success.jinja")
   
-    return render_template(template, form=form, key=None, form_type=formClass.__name__, submit_target=submit_target,
+    return render_template(template, form=form, key=None, form_type=form_class.__name__, submit_target=submit_target,
                            classes=("was-validated" if request.method == 'POST' else ''))
 
-def generic_edit(formClass, template, id=None, submit_target=None):
+def generic_edit(form_class, template, id=None, submit_target=None):
     """ Used for opening edit form and also POSTing values.
         Pattern used to reduce code duplication
     """
     object_id = id or request.values.get("key")
     
-    obj = formClass.Meta.model.query.get(object_id)
+    obj = form_class.Meta.model.query.get(object_id)
 
     if request.method == 'GET':
         if request.values.get("action") == "copy":
             obj.id = None
             object_id = None
-        form = formClass(obj=obj)
+        form = form_class(obj=obj)
     else:
-        form = formClass(request.values)
-
-    if form.validate_on_submit():
+        # use this way to prevent unique validation error while editing
+        form = form_class(obj=obj)
+        # form = form_class(request.values)
         form.populate_obj(obj)
+
+    if form.validate_on_submit():        
         obj.query.session.commit()
         flash('Data saved', category="Success")
         return render_template("form_success.jinja")
 
-    return render_template(template, form=form, key=object_id, form_type=formClass.__name__, submit_target=submit_target,
+    return render_template(template, form=form, key=object_id, form_type=form_class.__name__, submit_target=submit_target,
                            classes=("was-validated" if request.method == 'POST' else ''))
 
 def generic_delete(form, id):    
