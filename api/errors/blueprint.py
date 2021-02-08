127d093015fc6b33039231d4683b76e7618c77ad
Branimir <Branimir@BRANE-G5>
2021-02-08 10:24:49 +0100

    flash errors for inline edit

--- api/errors/blueprint.py
+++ api/errors/blueprint.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, render_template
+from flask.helpers import flash
 from marshmallow.exceptions import ValidationError
+from sqlalchemy.exc import IntegrityError, OperationalError
 
 errors = Blueprint('errors', __name__,
                    template_folder='templates',
@@ -20,3 +22,25 @@ def internal_server_error(e):
 def data_validation_error(e):
     validation_errors = e.args[0]
     return jsonify(validation_errors)
+
+@errors.app_errorhandler(IntegrityError)
+def sql_integrity_error(e):
+    description = e.args[0]
+    message = "Data related error occured"
+    if e.orig.args[0] == 2601:
+        message = "Duplicate data"
+    elif e.orig.args[0] == 515:
+        message = "Data is in use"
+
+    flash(message, category="Action failed")
+    return render_template("form_error.jinja")
+
+@errors.app_errorhandler(OperationalError)
+def sql_op_error(e):
+    description = e.args[0]
+    message = "Database related error occured"
+    if e.orig.args[0][0] == 18456:
+        message = "Database connection error. Try again or report problem."
+
+    flash(message, category="Action failed")
+    return render_template("form_error.jinja")
--- api/utils/template_filters.py
+++ api/utils/template_filters.py
@@ -3,6 +3,12 @@ def titlecase_label(fieldname):
         return ' '.join([w.title() for w in fieldname.split('_') if w != 'id'])
     return fieldname
 
+def ends_with_dot(text):
+    if not text.endswith("."):
+        return text + "."
+    return text
+
 def init_app(app):
   """Initialize a Flask application with custom filters."""
   app.jinja_env.filters['titlecase_label'] = titlecase_label
+  app.jinja_env.filters['end_dot'] = ends_with_dot
